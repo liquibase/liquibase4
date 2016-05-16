3117fb8e23b94f1efbcdfe954f1dbe404d45384e
Nathan Voxland <nathan@voxland.net>
2016-05-16 13:34:22 -0500

    Fixing tests for CommentAction

--- liquibase-common/src/test/groovy/liquibase/action/core/CommentActionTest.groovy
+++ liquibase-common/src/test/groovy/liquibase/action/core/CommentActionTest.groovy
@@ -1,11 +1,26 @@
 package liquibase.action.core
 
+import liquibase.Scope
+import liquibase.action.AbstractActionTest
+import liquibase.action.Action
+import liquibase.database.ConnectionSupplier
+import liquibase.snapshot.Snapshot
 import spock.lang.Specification
 
-class CommentActionTest extends Specification {
+class CommentActionTest extends AbstractActionTest {
 
     def "nothing to test"() {
         expect:
         assert true
+    }
+
+    @Override
+    List<Action> createAllActionPermutations(ConnectionSupplier connectionSupplier, Scope scope) {
+        return null;
+    }
+
+    @Override
+    protected Snapshot createSnapshot(Action action, ConnectionSupplier connectionSupplier, Scope scope) {
+        return null
     }
 }
--- liquibase-common/src/test/groovy/liquibase/actionlogic/core/CommentLogicTest.groovy
+++ liquibase-common/src/test/groovy/liquibase/actionlogic/core/CommentLogicTest.groovy
@@ -1,8 +1,22 @@
 package liquibase.actionlogic.core
 
+import liquibase.Scope
+import liquibase.action.AbstractActionTest
+import liquibase.actionlogic.AbstractActionLogicTest
+import liquibase.database.ConnectionSupplier
 import spock.lang.Specification
 
-class CommentLogicTest extends Specification {
+class CommentLogicTest extends AbstractActionLogicTest {
+
+    @Override
+    protected String collectAllValidationErrors(AbstractActionTest actionTest, ConnectionSupplier supplier, Scope scope) {
+        return null;
+    }
+
+    @Override
+    protected String getExpectedValidationErrors() {
+        return null
+    }
 
     def "nothing to test"() {
         expect:
